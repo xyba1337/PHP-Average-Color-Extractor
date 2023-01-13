88dc5a7150d736b3c17fae3a37860b003d29ffc9
xyba <62798507+xyba1337@users.noreply.github.com>
2023-01-13 05:22:16 +0100

    Create imgextractor.php

---
+++ imgextractor.php
@@ -0,0 +1,10 @@
+function getAvgColor($image) {
+    $img = imagecreatefromstring(file_get_contents($image));
+    $width = imagesx($img);
+    $height = imagesy($img);
+    $pixel = imagecreatetruecolor(1, 1);
+    imagecopyresampled($pixel, $img, 0, 0, 0, 0, 1, 1, $width, $height);
+    $rgb = imagecolorat($pixel, 0, 0);
+    $color = imagecolorsforindex($pixel, $rgb);
+    return sprintf('#%02x%02x%02x', $color['red'], $color['green'], $color['blue']);
+}
